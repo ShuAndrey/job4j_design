1b29f88c5a45deb9153fc519657c5073be213efa
AndreyShulgin <neonod404@gmail.com>
2022-06-14 18:03:43 +0300

    0. Что такое обобщенные типы (generics) [#4952 #7943]

---
+++ src/main/java/ru/job4j/generics/Predator.java
@@ -0,0 +1,9 @@
+package ru.job4j.generics;
+
+/**
+ * Модель данных.
+ *
+ * @author Andrey Shulgin
+ */
+public class Predator extends Animal {
+}
---
+++ src/main/java/ru/job4j/generics/Tiger.java
@@ -0,0 +1,9 @@
+package ru.job4j.generics;
+
+/**
+ * Модель данных.
+ *
+ * @author Andrey Shulgin
+ */
+public class Tiger extends Predator {
+}

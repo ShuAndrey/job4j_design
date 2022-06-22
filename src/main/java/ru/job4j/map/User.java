b5f22d6aa5cb674870e572ff2d3731a9f25578cc
AndreyShulgin <neonod404@gmail.com>
2022-06-22 21:28:42 +0300

    1. Создать модель User [#999 #7944]

---
+++ src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,20 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Модель данных.
+ *
+ * @author Andrey Shulgin
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}

777faf752e8bba8b5355ee0de2578e891842bf80
AndreyShulgin <neonod404@gmail.com>
2022-07-14 12:10:23 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,26 +16,32 @@ import java.net.Socket;
  * @author Andrey Shulgin
  */
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String msg = in.readLine();
-                    if (msg.contains("?msg=Exit")) {
-                        server.close();
-                    } else if (msg.contains("?msg=Hello")) {
-                        out.write("Hello, dear friend.".getBytes());
-                    } else {
-                        out.write("What".getBytes());
-                    }
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
-                    out.flush();
+    public static void main(String[] args) {
+        try {
+            try (ServerSocket server = new ServerSocket(9000)) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream();
+                         BufferedReader in = new BufferedReader(
+                                 new InputStreamReader(socket.getInputStream()))) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        String msg = in.readLine();
+                        if (msg.contains("?msg=Exit")) {
+                            server.close();
+                        } else if (msg.contains("?msg=Hello")) {
+                            out.write("Hello, dear friend.".getBytes());
+                        } else {
+                            out.write("What".getBytes());
+                        }
+
+                        out.flush();
+                    }
                 }
             }
+        } catch (IOException e) {
+            LOG.error("IOException", e);
         }
     }
 }
